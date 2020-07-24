4c947bf1116fb38e8be5e9b9762f83df271af811
Ronald Burrell Jr <ronburrell05@gmail.com>
2020-07-24 18:02:03 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -20,5 +20,5 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  'SELECT characters.name, COUNT(*) FROM characters INNER JOIN authors ON characters.author_id = authors.id JOIN series ON authors.id = series.author_id JOIN books ON series.id = books.series_id;'
+  'SELECT characters.name, COUNT(*) FROM character_books JOIN characters ON characters.author_id = authors.id JOIN series ON authors.id = series.author_id JOIN books ON series.id = books.series_id;'
 end
